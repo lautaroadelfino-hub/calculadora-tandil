f66510b05fb7f13917ea9b65f4c622c3bdacf97c
lautaroadelfino-hub <lautaroadelfino@gmail.com>
2025-11-09 14:33:23 -0300

    1.2

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',          // genera HTML estático
-  images: { unoptimized: true }, // evita Image Optimization (no hay server)
-};
-export default nextConfig;
